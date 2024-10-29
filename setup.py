268911243756d60550ac7bf0f7fad6be84ab6133
Filament-AI <filamentaiservices@gmail.com>
2024-10-29 18:47:09 +0530

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="productivity-timer",
-    version="1.0.0",
+    version="1.0.1",
+    packages=find_packages(), 
+
     description="A timer library for managing multiple timers for multiple individuals.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Abhishek",
     author_email="abhishekshivtiwari@gmail.com",
     url='https://github.com/Abhi-shekes/productivity-timer', 
-    packages=find_packages(),
     python_requires=">=3.6",
     install_requires=[],
     classifiers=[
